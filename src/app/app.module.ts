ab56b9ce4db4f4c7a5025c9db4e419f44724bb4a
Patrick <patrick.f.boyne@gmail.com>
2018-08-14 23:39:09 -0400

    added angular material cards and buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,14 @@ import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import {
+  MatButtonModule,
+  MatIconModule,
+  MatCardModule,
+  MatGridListModule,
+  MatSelectModule
+} from "@angular/material";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 
@@ -9,7 +17,7 @@ import { NavigationComponent } from "./navigation/navigation.component";
 import { SearchComponent } from "./search/search.component";
 import { CurrentPagePipe } from "./pipes/current-page.pipe";
 import { HomeComponent } from "./home/home.component";
-import { CharacterCardComponent } from './character-card/character-card.component';
+import { CharacterCardComponent } from "./character-card/character-card.component";
 
 @NgModule({
   declarations: [
@@ -20,7 +28,18 @@ import { CharacterCardComponent } from './character-card/character-card.componen
     CurrentPagePipe,
     CharacterCardComponent
   ],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule, FormsModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatGridListModule,
+    MatSelectModule,
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
